01909fada4d6784c8451826e08cf015416190f94
jiang-zhexin <162887873+jiang-zhexin@users.noreply.github.com>
2025-08-08 10:13:10 +0800

    fixes and improvements

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,11 @@
 import { Hono } from "hono";
 import { cache } from "hono/cache";
 import { etag } from "hono/etag";
-import { logger } from "hono/logger";
 import { vValidator } from "@hono/valibot-validator";
 
 import { ImageTransform } from "./schema/image_transform";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
-
-app.use(logger());
-
-app.use(async (c, next) => {
-  const cache = await caches.open("transformed-images");
-  const cacheResponse = await cache.match(c.req.url);
-  if (cacheResponse) {
-    return cacheResponse;
-  }
-  await next();
-});
 
 app.use(
   cache({
@@ -26,7 +14,11 @@ app.use(
   })
 );
 
-app.use(etag());
+app.use(
+  etag({
+    generateDigest: (body) => crypto.subtle.digest({ name: "SHA-256" }, body),
+  })
+);
 
 app.get("*", vValidator("query", ImageTransform), async (c) => {
   const key = c.req.path.slice(1);
@@ -38,6 +30,7 @@ app.get("*", vValidator("query", ImageTransform), async (c) => {
   if (Object.keys(opts).length === 0) {
     const headers = new Headers();
     obj.writeHttpMetadata(headers);
+    headers.set("etag", obj.httpEtag);
     return new Response(obj.body, { headers });
   }
   const result = await c.env.IMAGES.input(obj.body)
