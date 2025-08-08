6e24c2cf81b29ba9a1fc90eef4852714239c7996
jiang-zhexin <162887873+jiang-zhexin@users.noreply.github.com>
2025-08-08 10:13:10 +0800

    fixes and improvements

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { Hono } from "hono";
 import { cache } from "hono/cache";
-import { etag } from "hono/etag";
-import { logger } from "hono/logger";
 import { vValidator } from "@hono/valibot-validator";
 
 import { ImageTransform } from "./schema/image_transform";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
-
-app.use(logger());
 
 app.use(async (c, next) => {
   const cache = await caches.open("transformed-images");
@@ -26,8 +22,6 @@ app.use(
   })
 );
 
-app.use(etag());
-
 app.get("*", vValidator("query", ImageTransform), async (c) => {
   const key = c.req.path.slice(1);
   const obj = await c.env.BUCKET.get(key);
@@ -38,6 +32,7 @@ app.get("*", vValidator("query", ImageTransform), async (c) => {
   if (Object.keys(opts).length === 0) {
     const headers = new Headers();
     obj.writeHttpMetadata(headers);
+    headers.set("etag", obj.httpEtag);
     return new Response(obj.body, { headers });
   }
   const result = await c.env.IMAGES.input(obj.body)
